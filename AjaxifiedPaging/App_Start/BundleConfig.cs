c287fc0dc96cc350500f21d9772cef4ab1f8fe8a
Karthik Ananthapadmanaban <karthika25@gmail.com>
2016-04-03 00:56:59 -0400

    initial commit of ajax based paging

---
+++ AjaxifiedPaging/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Optimization;
+
+namespace AjaxifiedPaging
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/bootstrap.js", "~/Scripts/mustache.js", "~/Scripts/mustache-wax.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css", "~/Content/bootstrap.css"));
+        }
+    }
+}
---
+++ AjaxifiedPaging/Scripts/paging-core.js
@@ -0,0 +1,64 @@
+﻿// after success adjust the page url
+// like /c/manage/!<page_no>
+$(function () {
+    if ($('script[type="x-tmpl-mustache"]') && typeof (paging_options) === "object") {
+        $.ajax({
+            type: 'post',
+            url: paging_options.GetUrl,
+            data: { 'pageContentRequest': JSON.stringify(paging_options) },
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+                paging_options = data;
+                renderTableData(data);
+                renderPagerData(data);
+                setupPager();
+            }
+        });
+    }    
+});
+
+function setupPager() {
+    $('.page-entry').on('click', function (e) {
+        e.preventDefault();
+        var pageNo = $(this).data('page-no');
+        paging_options.Paging.CurrentPage = pageNo;
+        paging_options.Entries = [],
+        console.log(paging_options);
+        $.ajax({
+            type: 'post',
+            url: paging_options.GetUrl,
+            data: { 'pageContentRequest': JSON.stringify(paging_options) },
+            dataType: 'json',
+            success: function (data) {
+                paging_options = data;
+                renderTableData(data);
+                renderPagerData(data);
+                setupPager();
+                window.location.hash = '!/' + pageNo;
+            }
+        });
+    });
+}
+
+function renderTableData(options) {
+    var template = $('#template').html();
+    Mustache.parse(template);
+    Mustache.Formatters = {
+        date: function (str) {
+            var dt = new Date(parseInt(str.substr(6, str.length - 8), 10));
+            return (dt.getDate() + "/" + (dt.getMonth() + 1) + "/" + dt.getFullYear());
+        }
+    };
+    var rendered = Mustache.render(template, {
+        Entries: options.Entries
+    });
+    $('#' + options.Container).html(rendered);
+}
+
+function renderPagerData(options) {
+    var pagerTemplate = $('#pagerTemplate').html();
+    Mustache.parse(pagerTemplate);
+    var rendered = Mustache.render(pagerTemplate);
+    $('#pager').html(rendered);
+}
